7d6967e3af99404b4fbdc6c2a2996777882b2400
3li07 <105653810+3li07@users.noreply.github.com>
2022-08-06 11:50:55 +0300

    Update layout.php

--- examenphp/app/Views/layout.php
+++ examenphp/app/Views/layout.php
@@ -6,6 +6,11 @@ $table = $id_Table[1];
 $professeurTable = app::getInstance()->getTable('Professeur');
 $etudiantTable = app::getInstance()->getTable('Etudiant');
 $messageFlash = null;
+$profexist = $professeurTable->getList();
+$vide =false;
+ if(empty($profexist)){
+    $vide = true;
+ }
  if($table === 'etudiants'){
   $utilisateur = $etudiantTable->getById($id);
   $nom = $utilisateur->nom . " ". $utilisateur->prenom;
@@ -121,7 +126,7 @@ $messageFlash = null;
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="?p=modules.index">Liste Modules</a>
-                        <a class="collapse-item" href="?p=modules.add">Ajouter un Modules</a>
+                        <?php if(!$vide) : ?><a class="collapse-item" href="?p=modules.add">Ajouter un Modules</a><?php endif ?>
                     </div>
                 </div>
             </li>
@@ -163,10 +168,6 @@ $messageFlash = null;
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
